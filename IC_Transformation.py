45ac3d00f67afb5d57b456411d41c3150abeda11
Derrick Yang <yxt1991@gmail.com>
2015-10-05 21:27:07 -0400

    target_ic and ic_difference bugs fixed
    ic , target_ic, ic_defference change into numpy array.

--- IC_Transformation.py
+++ IC_Transformation.py
@@ -17,7 +17,7 @@ class IC_Transformation(object):
      | ``coordinates`` -- Numpy.array,  shape (N, 3); cartesian coordinates of molecule.
      | ``init_coordinates -- Numpy.array,  shape (N, 3); initial cartesian coordinates.
      | ``numbers`` -- Numpy.array, shape (N,); a array of atomic number of each atoms of target molecule.
-     | ``ic`` -- List; a list of internal coordinates calculated through transformation.
+     | ``ic`` -- Numpy.array, shape(n,); a list of internal coordinates calculated through transformation.
      | ``iteration_falg`` -- Bool; a flag to mark is it doing iteration or generate new ic. T for iteration, F for new ic.
      | ``procedures`` -- List; steps used to generate ic which will be used when doing iteration
      | ``bond`` -- List; list of added ic bond set
@@ -32,15 +32,15 @@ class IC_Transformation(object):
         self.init_coordinates = molecule.coordinates
         self.numbers = molecule.numbers
         self.len = len(self.numbers)
-        self.ic = []
+        self.ic = np.array([])
         self.iteration_flag = False
         self.procedures = []
         self.bond = []
         self.angle = []
         self.dihed = []
         self.B_matrix = np.zeros((0,3*self.len),float)
-        self._ic_differences = []
-        self._target_ic = []
+        self._ic_differences = np.array([])
+        self._target_ic = np.array([])
         self.H_matrix = np.zeros((0, 3*self.len, 3*self.len), float)
 
 
@@ -173,7 +173,7 @@ class IC_Transformation(object):
         result, d, dd = ic_function(coordinates, deriv = 2)
         if not self.iteration_flag:
             self.procedures.append(procedures)
-        self.ic.append(result)
+        self.ic = np.append( self.ic, result)
         self._add_B_matrix(d, atoms)
         self._add_H_matrix(dd, atoms)
 
@@ -228,17 +228,22 @@ class IC_Transformation(object):
         """getter of self.ic_differences. To calculate ic difference between present ic value and target ic value.
         return a list.
         """
-        self._ic_differences.extend([0]*(len(self.ic) - len(self._ic_differences)))
-        return list(np.array(self.ic) - np.array(self.target_ic))
+        if len(self._ic_differences) < len(self.ic):
+            self._ic_differences = np.append(self._ic_differences, np.zeros( (len(self.ic) - len(self._ic_differences)),float))
+        return self.ic - self.target_ic
 
 
     def _set_difference(self, value):
         """setter for self.target. Use the value of difference and present ic to calculate target ic value.
 
         Arguments:
-         | ``value`` List; list of ic.difference value
+         | ``value`` Numpy.array or List; an array of ic.difference value
         """
-        self.target_ic = list(np.array(self.ic) + np.array(value))
+        value = np.array(value)
+        if len(value) <= len(self.ic):
+            value = np.append(value, np.zeros( (len(self.ic) - len(value)),float))
+        else:raise AtomsNumberError
+        self.target_ic = self.ic - value
 
 
     ic_differences = property(_get_difference, _set_difference)
@@ -248,18 +253,21 @@ class IC_Transformation(object):
         """getter for self.target.
         """
         if len(self._target_ic) < len(self.ic):
-            self._target_ic.extend(self.ic[len(self._target_ic):])
-        return self._target_ic or self.ic
+            self._target_ic = np.append( self._target_ic, self.ic[len(self._target_ic):])
+        return self._target_ic
 
 
     def _set_target_ic(self, value):
         """setter for self.target
 
         Arguments:
-         | ``value`` List; a list of target ic value.
+         | ``value`` Numpy.array or List; an array of target ic value.
         """
-        if len(value) == len(self.ic):
-            self._target_ic = value
+        value = np.array(value)
+        if len(value) <= len(self.ic):
+            self._target_ic = np.append(value, self.ic[len(value):])
+        else:
+            raise AtomsNumberError
 
 
     target_ic = property(_get_target_ic, _set_target_ic)
@@ -348,7 +356,7 @@ class IC_Transformation(object):
         """private method to calculate each internal coordinates again to get the updated ic value for new coordinates
         """
         self.iteration_flag = True
-        self.ic = []
+        self.ic = np.array([])
         self.B_matrix = np.zeros((0,3*self.len),float)
         self.H_matrix = np.zeros((0, 3*self.len, 3*self.len), float)
         for i in self.procedures:
@@ -395,33 +403,80 @@ class IC_Transformation(object):
 
 
 if __name__ == '__main__':
-    fn_xyz = ht.context.get_fn("test/water.xyz")
+    fn_xyz = ht.context.get_fn("test/2h-azirine.xyz")
     mol = ht.IOData.from_file(fn_xyz)
-    water = IC_Transformation(mol)
-    print water.coordinates
-    print water.numbers.shape
-    water.add_bond_length(0,1)
-    water.add_bond_length(1,2)
-    water.add_bend_angle(0,1,2)
-    print water.ic
-    water.target_ic = [1.5,1.5,1.5006340153991836]
-    print water.ic_differences
-    a,b,c = water.calculate_cost()
-    print a, b, c
-    firstP = Point(water.coordinates.reshape(1,-1), a, b, c)
-    opwater = DOM(firstP, water, IC_Transformation.new_coor_acceptor)
-    print opwater.p0.point.value
-    print "__"
-    opwater.first_step()
-    print water.coordinates
-    print water.ic
-    opwater.first_step()
-    print opwater.p1.stepratio
-    opwater.second_step()
-    print opwater.p1.point.coordinates
-    print np.dot(opwater.p1.point.first_deriv, opwater.p1.point.first_deriv)
-    print water.coordinates
-    print water.ic
+    h2a = IC_Transformation(mol)
+    h2a.add_bond_length(0, 1)
+    h2a.add_bond_length(1, 2)
+    h2a.add_bond_length(0, 2)
+    h2a.add_bond_length(2, 5)
+    h2a.add_bond_length(1,3)
+    h2a.add_bond_length(1,4)
+
+    h2a.add_bend_angle(0,2,1)
+    h2a.add_bend_angle(0,1,2)
+    h2a.add_bend_angle(0,2,5)
+    h2a.add_bend_angle(0,1,3)
+    h2a.add_bend_angle(0,1,4)
+
+    h2a.add_dihed_angle(0,1,3,4)
+
+    print h2a.ic
+    h2a.target_ic = [2.7, 2.5, 2.3, 2.0]
+    print h2a.target_ic
+    a,b,c = h2a.calculate_cost()
+    # print np.linalg.inv(c)
+    print b
+    firstP = Point(h2a.coordinates.reshape(1,-1), a, b, c)
+    oph2a = DOM(firstP, h2a,IC_Transformation.new_coor_acceptor)
+    print h2a.coordinates
+    print h2a.ic
+    oph2a.first_step()
+    print h2a.coordinates
+    print h2a.ic
+
+
+    # fn_xyz = ht.context.get_fn("test/water.xyz")
+    # mol = ht.IOData.from_file(fn_xyz)
+    # water = IC_Transformation(mol)
+    # print water.coordinates
+    # print water.numbers.shape
+    # water.add_bond_length(0,1)
+    # water.add_bond_length(1,2)
+    # water.add_bend_angle(0,1,2)
+    # print water.ic
+    # water.target_ic = [1.5,1.5]
+    # # print water.target_ic
+    # # print water.ic_differences
+    # # water.ic_differences = [0.1,0.1]
+    # # print water.target_ic
+    # # print water.ic_differences
+    # # water.add_bend_angle(1,0,2)
+    # # print water.ic
+    # # print water.target_ic
+    # # print water.ic_differences
+    # print water.ic_differences
+    # a,b,c = water.calculate_cost()
+    # print b, "this is b" 
+    # firstP = Point(water.coordinates.reshape(1,-1), a, b, c)
+    # opwater = DOM(firstP, water, IC_Transformation.new_coor_acceptor)
+    # print opwater.p0.point.value
+    # print "__"
+    # opwater.first_step()
+    # print water.coordinates
+    # print water.ic
+    # opwater.first_step()
+    # print opwater.p1.stepratio
+    # opwater.second_step()
+    # print opwater.p1.point.coordinates
+    # print np.dot(opwater.p1.point.first_deriv, opwater.p1.point.first_deriv)
+    # print water.coordinates
+    # print water.ic
+
+
+
+
+
     # print water.coordinates.shape
     # fn_xyz = ht.context.get_fn('test/2h-azirine.xyz')
     # mol = ht.IOData.from_file(fn_xyz)
