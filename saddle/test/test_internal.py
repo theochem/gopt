68fa7fada0ab42cbde440068da8107764316bf9e
Derrick <yxt1991@gmail.com>
2017-04-15 22:48:58 -0400

    change internal test
    1. remove setupclass
    2. add setUp for testclass
    3. remove duplicate deepcopy

--- saddle/test/test_internal.py
+++ saddle/test/test_internal.py
@@ -9,29 +9,26 @@ from ..opt import Point
 
 
 class TestInternal(object):
-    @classmethod
-    def setup_class(self):
+    def setUp(self):
         path = os.path.dirname(os.path.realpath(__file__))
         mol_path = path + "/../data/water.xyz"
         mol = IOData.from_file(mol_path)
-        self.internal = Internal(mol.coordinates, mol.numbers, 0, 1)
+        self.mol = Internal(mol.coordinates, mol.numbers, 0, 1)
 
     def test_connectivity(self):
-        mol = deepcopy(self.internal)
-        assert np.allclose(mol.connectivity, np.eye(3) * -1)
+        assert np.allclose(self.mol.connectivity, np.eye(3) * -1)
 
     def test_add_bond(self):
-        mol = deepcopy(self.internal)
         init_con = np.eye(3) * -1
-        assert np.allclose(mol.connectivity, init_con)
-        mol.add_bond(1, 0)
+        assert np.allclose(self.mol.connectivity, init_con)
+        self.mol.add_bond(1, 0)
         new_con = init_con.copy()
         new_con[0, 1] = 1
         new_con[1, 0] = 1
-        assert np.allclose(mol.connectivity, new_con)
-        assert np.allclose(mol.ic_values, np.array([1.81413724]))
-        assert mol.ic[0].atoms == (0, 1)
-        mol.add_bond(0, 1)
+        assert np.allclose(self.mol.connectivity, new_con)
+        assert np.allclose(self.mol.ic_values, np.array([1.81413724]))
+        assert self.mol.ic[0].atoms == (0, 1)
+        self.mol.add_bond(0, 1)
         ref_hessian = np.array([
             [[0.18374187, 0.25985046, 0., -0.18374187, -0.25985046, 0., 0., 0.,
               0.], [0.25985046, 0.36748434, 0., -0.25985046, -0.36748434, 0.,
@@ -45,33 +42,32 @@ class TestInternal(object):
              [0., 0., 0., 0., 0., 0., 0., 0., 0.],
              [0., 0., 0., 0., 0., 0., 0., 0., 0.]]
         ])
-        assert np.allclose(mol._cc_to_ic_hessian, ref_hessian)
-        assert len(mol.ic) == 1
-        assert np.allclose(mol.connectivity, new_con)
-        mol.add_bond(2, 1)
-        assert len(mol.ic) == 2
-        assert mol.ic[1].atoms == (1, 2)
-        assert np.allclose(mol._cc_to_ic_gradient, np.array(
+        assert np.allclose(self.mol._cc_to_ic_hessian, ref_hessian)
+        assert len(self.mol.ic) == 1
+        assert np.allclose(self.mol.connectivity, new_con)
+        self.mol.add_bond(2, 1)
+        assert len(self.mol.ic) == 2
+        assert self.mol.ic[1].atoms == (1, 2)
+        assert np.allclose(self.mol._cc_to_ic_gradient, np.array(
             [[0.81649681, -0.57734995, 0., -0.81649681, 0.57734995, 0., 0., 0.,
               0.], [0., 0., 0., 0.81649681, 0.57734995, 0., -0.81649681,
                     -0.57734995, 0.]]))
 
     def test_angle_add(self):
-        mol = deepcopy(self.internal)
-        mol.add_angle_cos(0, 1, 2)
-        assert len(mol.ic) == 0
-        mol.add_bond(0, 1)
-        mol.add_bond(1, 2)
-        connected_index = mol.connected_indices(1)
+        self.mol.add_angle_cos(0, 1, 2)
+        assert len(self.mol.ic) == 0
+        self.mol.add_bond(0, 1)
+        self.mol.add_bond(1, 2)
+        connected_index = self.mol.connected_indices(1)
         assert np.allclose(connected_index, np.array([0, 2]))
-        mol.add_angle_cos(0, 1, 2)
-        assert len(mol.ic) == 3
-        assert np.allclose(mol.ic[2].value, -0.33333406792305265)
-        assert np.allclose(mol._cc_to_ic_gradient[2], np.array(
+        self.mol.add_angle_cos(0, 1, 2)
+        assert len(self.mol.ic) == 3
+        assert np.allclose(self.mol.ic[2].value, -0.33333406792305265)
+        assert np.allclose(self.mol._cc_to_ic_gradient[2], np.array(
             [-0.3000493, -0.42433414, 0., 0., 0.84866827, -0., 0.3000493,
              -0.42433414, 0.]))
-        # print mol._cc_to_ic_hessian[2]
-        assert np.allclose(mol._cc_to_ic_hessian[2], np.array([
+        # print self.mol._cc_to_ic_hessian[2]
+        assert np.allclose(self.mol._cc_to_ic_hessian[2], np.array([
             [0.30385023, 0.1432367, 0., -0.27008904, -0.19098226, -0.,
              -0.03376119, 0.04774557, 0.],
             [0.1432367, -0.20256656, 0., -0.09549113, 0.13504407, -0.,
@@ -87,8 +83,8 @@ class TestInternal(object):
              -0.1432367, -0.20256656, 0.], [0., 0., 0.30385034, -0., -0.,
                                             -0.40513401, 0., 0., 0.10128367]
         ]))
-        mol.set_target_ic((1.6, 1.7, -0.5))
-        assert np.allclose(mol.target_ic, np.array([1.6, 1.7, -0.5]))
+        self.mol.set_target_ic((1.6, 1.7, -0.5))
+        assert np.allclose(self.mol.target_ic, np.array([1.6, 1.7, -0.5]))
 
     def test_dihedral_add(self):
         path = os.path.dirname(os.path.realpath(__file__))
@@ -147,24 +143,23 @@ class TestInternal(object):
                            ref_hessian)
 
     def test_cost_function(self):
-        mol = deepcopy(self.internal)
-        mol.add_bond(0, 1)
-        mol.add_bond(1, 2)
-        mol.add_angle_cos(0, 1, 2)
+        self.mol.add_bond(0, 1)
+        self.mol.add_bond(1, 2)
+        self.mol.add_angle_cos(0, 1, 2)
         assert np.allclose(
-            mol.ic_values,
+            self.mol.ic_values,
             [1.8141372422079882, 1.8141372422079882, -0.33333406792305265])
-        mol.set_target_ic([1.7, 1.7, -0.4])
-        mol.swap_internal_coordinates(0, 2)
+        self.mol.set_target_ic([1.7, 1.7, -0.4])
+        self.mol.swap_internal_coordinates(0, 2)
         assert np.allclose(
-            mol.ic_values,
+            self.mol.ic_values,
             [-0.33333406792305265, 1.8141372422079882, 1.8141372422079882])
-        mol.swap_internal_coordinates(0, 2)
+        self.mol.swap_internal_coordinates(0, 2)
         assert np.allclose(
-            mol.ic_values,
+            self.mol.ic_values,
             [1.8141372422079882, 1.8141372422079882, -0.33333406792305265])
-        assert np.allclose(mol.target_ic, [1.7, 1.7, -0.4])
-        v, d, dd = mol._cost_value()
+        assert np.allclose(self.mol.target_ic, [1.7, 1.7, -0.4])
+        v, d, dd = self.mol._cost_value()
         assert np.allclose(0.030498966617378116, v)
         ref_gradient = np.array(
             [0.22827448441597653, 0.22827448441597653, 0.13333186415389475])
@@ -178,23 +173,23 @@ class TestInternal(object):
                                                  2., ]])
         assert np.allclose(dd, ref_hessian)
         # assert False
-        new_v, xd, xdd = mol.cost_value_in_cc
+        new_v, xd, xdd = self.mol.cost_value_in_cc
         assert new_v == v
-        assert np.allclose(xd, np.dot(mol._cc_to_ic_gradient.T, d))
+        assert np.allclose(xd, np.dot(self.mol._cc_to_ic_gradient.T, d))
         ref_x_hessian = np.dot(
-            np.dot(mol._cc_to_ic_gradient.T, dd), mol._cc_to_ic_gradient)
-        K = np.tensordot(d, mol._cc_to_ic_hessian, 1)
+            np.dot(self.mol._cc_to_ic_gradient.T, dd), self.mol._cc_to_ic_gradient)
+        K = np.tensordot(d, self.mol._cc_to_ic_hessian, 1)
         ref_x_hessian += K
         assert np.allclose(xdd, ref_x_hessian)
         new_coor = np.array([
             [1.40, -0.93019123, -0.], [-0., 0.11720081, -0.],
             [-1.40, -0.93019123, -0.]
         ])
-        mol.set_new_coordinates(new_coor)
+        self.mol.set_new_coordinates(new_coor)
         assert np.allclose(
-            mol.ic_values,
+            self.mol.ic_values,
             [1.7484364736491811, 1.7484364736491811, -0.28229028459335431])
-        assert np.allclose(mol._cc_to_ic_gradient[0, :6], np.array(
+        assert np.allclose(self.mol._cc_to_ic_gradient[0, :6], np.array(
             [0.80071539, -0.59904495, 0., -0.80071539, 0.59904495, -0.]))
         ref_hessian = np.array([
             [0.20524329, 0.27433912, 0., -0.20524329, -0.27433912,
@@ -204,25 +199,23 @@ class TestInternal(object):
              0.], [-0.27433912, -0.36669628, -0., 0.27433912, 0.36669628,
                    0.], [-0., -0., -0.57193957, 0., 0., 0.57193957]
         ])
-        assert np.allclose(mol._cc_to_ic_hessian[0, :6, :6], ref_hessian)
+        assert np.allclose(self.mol._cc_to_ic_hessian[0, :6, :6], ref_hessian)
 
     def test_transform_function(self):
-        mol = deepcopy(self.internal)
-        mol.add_bond(0, 1)
-        mol.add_bond(1, 2)
-        mol.add_angle_cos(0, 1, 2)
-        mol.set_target_ic([1.7, 1.7, -0.4])
-        n_p = mol._create_geo_point()
+        self.mol.add_bond(0, 1)
+        self.mol.add_bond(1, 2)
+        self.mol.add_angle_cos(0, 1, 2)
+        self.mol.set_target_ic([1.7, 1.7, -0.4])
+        n_p = self.mol._create_geo_point()
         assert isinstance(n_p, Point)
         assert n_p.trust_radius == 1.7320508075688772
-        mol.converge_to_target_ic(iteration=100)
-        g_array = mol.cost_value_in_cc[1]
+        self.mol.converge_to_target_ic(iteration=100)
+        g_array = self.mol.cost_value_in_cc[1]
         assert len(g_array[abs(g_array) > 3e-6]) == 0
 
     def test_auto_ic_select_water(self):
-        mol = deepcopy(self.internal)
-        mol.auto_select_ic()
-        assert np.allclose(mol.ic_values,
+        self.mol.auto_select_ic()
+        assert np.allclose(self.mol.ic_values,
                            [1.8141372422079882, 1.8141372422079882,
                             -0.33333406792305265])
 
@@ -243,19 +236,18 @@ class TestInternal(object):
         assert len(methanol.ic) == 15
 
     def test_wipe_ic(self):
-        mol = deepcopy(self.internal)
-        mol.add_bond(0, 1)
-        mol.add_bond(1, 2)
-        mol.add_bond(0, 2)
-        assert len(mol.ic) == 3
-        mol.wipe_ic_info(False)
-        assert len(mol.ic) == 3
-        mol.wipe_ic_info(True)
-        assert len(mol.ic) == 0
+        self.mol.add_bond(0, 1)
+        self.mol.add_bond(1, 2)
+        self.mol.add_bond(0, 2)
+        assert len(self.mol.ic) == 3
+        self.mol.wipe_ic_info(False)
+        assert len(self.mol.ic) == 3
+        self.mol.wipe_ic_info(True)
+        assert len(self.mol.ic) == 0
 
     def test_set_new_ic(self):
-        mol1 = deepcopy(self.internal)
-        mol2 = deepcopy(self.internal)
+        mol1 = deepcopy(self.mol)
+        mol2 = deepcopy(self.mol)
         mol1.add_bond(0, 1)
         mol1.add_bond(0, 2)
         mol1.add_angle_cos(1, 0, 2)
@@ -271,21 +263,20 @@ class TestInternal(object):
     def test_get_energy_from_fchk(self):
         path = os.path.dirname(os.path.realpath(__file__))
         fchk_path = path + "/../data/water_1.fchk"
-        mole = deepcopy(self.internal)
-        mole.add_bond(0, 1)
-        mole.add_bond(1, 2)
-        mole.add_angle_cos(0, 1, 2)
-        mole.energy_from_fchk(fchk_path)
-        ref_g = mole.internal_gradient.copy()
-        assert np.allclose(mole.internal_gradient, ref_g)
-        ic_ref = deepcopy(mole.ic)
-        mole.add_bond(0, 2)
-        assert not np.allclose(mole.internal_gradient.shape, ref_g.shape)
-        mole.set_new_ics(ic_ref)
-        assert np.allclose(mole.internal_gradient, ref_g)
-        mole.swap_internal_coordinates(0, 2)
-        assert np.allclose(mole.internal_gradient[2], ref_g[0])
-        assert np.allclose(mole.internal_gradient[0], ref_g[2])
+        self.mol.add_bond(0, 1)
+        self.mol.add_bond(1, 2)
+        self.mol.add_angle_cos(0, 1, 2)
+        self.mol.energy_from_fchk(fchk_path)
+        ref_g = self.mol.internal_gradient.copy()
+        assert np.allclose(self.mol.internal_gradient, ref_g)
+        ic_ref = deepcopy(self.mol.ic)
+        self.mol.add_bond(0, 2)
+        assert not np.allclose(self.mol.internal_gradient.shape, ref_g.shape)
+        self.mol.set_new_ics(ic_ref)
+        assert np.allclose(self.mol.internal_gradient, ref_g)
+        self.mol.swap_internal_coordinates(0, 2)
+        assert np.allclose(self.mol.internal_gradient[2], ref_g[0])
+        assert np.allclose(self.mol.internal_gradient[0], ref_g[2])
 
     def test_delete_ic(self):
         path = os.path.dirname(os.path.realpath(__file__))
