3a35f118b667669a0f4e535172c88c0691d8e78a
Derrick <yxt1991@gmail.com>
2017-08-23 23:32:20 -0400

    add procrustes
    1. add transilation
    2. add rotation
    3. add test

---
+++ saddle/procrustes/procrustes.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import, print_function
+
+import numpy as np
+
+from saddle.periodic import periodic
+from saddle.periodic.units import amu
+
+
+class Procrustes(object):
+    def __init__(self, target, *candidates):
+        assert len(candidates) > 0
+        assert all(
+            np.array_equal(target.numbers, i.numbers) for i in candidates)
+        self._target = target
+        self._candidates = candidates
+        self._target_center = Procrustes._barycenter(target.coordinates,
+                                                     target.numbers)
+
+    @staticmethod
+    def _barycenter(coordinates, numbers):
+        assert isinstance(coordinates, np.ndarray)
+        assert isinstance(numbers, np.ndarray)
+        assert numbers.ndim == 1
+        assert coordinates.ndim == 2
+        fac_ato_ma = np.vectorize(Procrustes._fetch_atomic_mass)
+        atom_mass = fac_ato_ma(numbers)
+        return np.einsum('i,ij->j', atom_mass, coordinates) / np.sum(atom_mass)
+
+    @staticmethod
+    def _fetch_atomic_mass(atomic_num):
+        return periodic[atomic_num].mass / amu
+
+    @staticmethod
+    def _move_to_center(coordinates, numbers):
+        center = Procrustes._barycenter(coordinates, numbers)
+        return coordinates - center
+
+    @staticmethod
+    def _rotate_coordinates(co1, co2):
+        assert isinstance(co1, np.ndarray)
+        assert isinstance(co2, np.ndarray)
+        assert co1.shape == co2.shape
+        if co1.ndim == 1:
+            op = np.outer(co1, co2)
+        else:
+            op = np.dot(co1.T, co2)
+        u, s, v = np.linalg.svd(op)
+        # return np.dot(np.dot(u, v), co2.T).T
+        return np.dot(co2, np.dot(u, v).T)
+
+    def rotate_mols(self):
+        adj_tar_coor = Procrustes._move_to_center(self._target.coordinates,
+                                                  self._target.numbers)
+        for i in self._candidates:
+            adj_center = Procrustes._move_to_center(i.coordinates, i.numbers)
+            adj_can_coor = Procrustes._rotate_coordinates(
+                adj_tar_coor, adj_center)
+            yield adj_can_coor + self._target_center
---
+++ saddle/procrustes/test/test_procrustes.py
@@ -0,0 +1,92 @@
+import os
+
+import numpy as np
+
+from saddle.periodic import periodic
+from saddle.periodic.units import amu
+from saddle.procrustes.procrustes import Procrustes
+from saddle.conf import data_dir
+from saddle.iodata import IOData
+from copy import deepcopy
+from collections import Iterable
+
+
+class test_procrustes(object):
+    def test_barycenter(self):
+        ori_numbers = np.array([1, 2])
+        m1 = periodic[1].mass / amu
+        m2 = periodic[2].mass / amu
+        mass_numbers = np.array([m1, m2])
+        assert np.allclose(mass_numbers, np.array([1, 4]), atol=1.e-2)
+        coordinates = np.array([[0, 1, 0], [1, 0, 0]])
+        result = np.einsum('i, ij -> j', mass_numbers, coordinates)
+        assert np.allclose(
+            Procrustes._barycenter(coordinates, ori_numbers),
+            result / np.sum(mass_numbers))
+
+    def test_fetch_atomic_amass(self):
+        assert Procrustes._fetch_atomic_mass(1) - 1.007975 < 1e-5
+        assert Procrustes._fetch_atomic_mass(6) - 12.0106 < 1e-5
+
+    def test_move_center(self):
+        numbers = np.array([1, 2])
+        coordinates = np.array([[0, 1, 0], [1, 0, 0]])
+        coor_1st = Procrustes._move_to_center(coordinates, numbers)
+        coordinates_2 = np.array([[3, 1, 0], [4, 0, 0]])
+        coor_2nd = Procrustes._move_to_center(coordinates_2, numbers)
+        assert np.allclose(coor_1st, coor_2nd)
+
+        np.random.seed(10)  # set random number seed
+        extra = np.random.rand(3)
+        coordinates_3 = coordinates + extra
+        coor_3rd = Procrustes._move_to_center(coordinates_3, numbers)
+        assert np.allclose(coor_1st, coor_3rd)
+
+        extra_2 = np.random.rand(3)
+        coordinates_4 = coordinates_3 + extra_2
+        coor_4th = Procrustes._move_to_center(coordinates_4, numbers)
+        assert np.allclose(coor_1st, coor_4th)
+
+    def test_rotate_coordiantes(self):
+        coordinates = np.array([0, 1, 0])
+        coordinates_2 = np.array([-1, 0, 0])
+        coor_2 = Procrustes._rotate_coordinates(coordinates, coordinates_2)
+        assert np.allclose(coordinates, coor_2)
+
+        coordinates_2d = np.array([[1, 1, 0], [-1, 1, 0]])
+        coordinates_2d_2 = np.array([[-1, 1, 0], [-1, -1, 0]])
+        coor_2d_2 = Procrustes._rotate_coordinates(coordinates_2d,
+                                                   coordinates_2d_2)
+        assert np.allclose(coordinates_2d, coor_2d_2)
+
+        coordinates_2d_3 = np.array([[-1, 0, 1], [-1, 0, -1]])
+        coor_2d_3 = Procrustes._rotate_coordinates(coordinates_2d,
+                                                   coordinates_2d_3)
+        assert np.allclose(coordinates_2d, coor_2d_3)
+
+    def test_move_and_rotate(self):
+        numbers = np.array([1, 2])
+        coordinates = np.array([[0, 1, 0], [1, 0, 0]])
+        coordinates_2 = np.array([[0, 1, 1], [1, 2, 1]])
+        center_1 = Procrustes._move_to_center(coordinates, numbers)
+        center_2_tmp = Procrustes._move_to_center(coordinates_2, numbers)
+        center_2 = Procrustes._rotate_coordinates(center_1, center_2_tmp)
+        assert np.allclose(center_1, center_2)
+
+    def test_main_function(self):
+        file_path = os.path.join(data_dir, "water.xyz")
+        water = IOData.from_file(file_path)
+        water.coordinates = np.array([[0, 1, 0], [1, 0, 0], [-1, -1, 1]])
+        water_2 = deepcopy(water)
+        water_2.coordinates = np.array([[-1, 0, 0], [0, 1, 0], [1, -1, 0]])
+        water_2.coordinates += 1.
+        water_3 = deepcopy(water)
+        water_3.coordinates = np.array([[1, 0, 0], [0, -1, 0], [-1, 1, 0]])
+        water_3.coordinates -= 1.
+
+        pcs = Procrustes(water, water_2, water_3)
+        final_xyz = pcs.rotate_mols()
+        assert isinstance(final_xyz, Iterable)
+        assert len(list(final_xyz)) == 2
+        for i in final_xyz:
+            assert np.allclose(i, water.coordinates)
