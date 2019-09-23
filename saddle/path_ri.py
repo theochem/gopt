41a5d727d09c37b77bce0af289faef89040a902a
Derrick <yxt1991@gmail.com>
2019-09-22 21:45:02 -0400

    Update path_ri
    1. Set num_key_ic to 0
    2. Add methods to check new vspace is in proper shape
    3. Update tests

--- saddle/path_ri.py
+++ saddle/path_ri.py
@@ -9,6 +9,22 @@ from saddle.reduced_internal import ReducedInternal
 
 
 class PathRI(ReducedInternal):
+    def __init__(self, coordinates, numbers, charge, multi, path_vector, title=""):
+        super().__init__(coordinates, numbers, charge, multi, title, key_ic_number=0)
+        if len(path_vector.shape) == 1:
+            raise ValueError("Path vector is a 1d array")
+        self._path_vector = path_vector
+        self._k_ic_n = 0
+
+    @property
+    def path_vector(self):
+        return self._path_vector
+
+    @property
+    def real_unit_path_vector(self):
+        tfm = np.dot(self.b_matrix, np.linalg.pinv(self.b_matrix))
+        real_path_v = np.dot(tfm, self.path_vector)
+        return real_path_v / np.linalg.norm(real_path_v)
 
     def set_path_vector(self, vector):
         assert isinstance(vector, np.ndarray)
@@ -21,50 +37,76 @@ class PathRI(ReducedInternal):
     def select_key_ic(self, *indices):
         raise NotImplementedError
 
-    def _reduced_unit_vectors(self):  # tested
-        raise NotImplementedError
-
     @classmethod
     def update_to_reduced_internal(cls, internal_ob, key_ic_number=0):
         raise NotImplementedError
 
-    def _reduced_perturbation(self):
-        tsfm = np.dot(self.b_matrix,
-                      pse_inv(self.b_matrix))
-        result = np.dot(tsfm, self._path_vector.T)
-        assert len(result.shape) == 1
-        return result[:, None]
+    def _svd_of_b_matrix(self, threshold=1e-6) -> "np.ndarray":  # tested
+        b_space = np.dot(self.b_matrix, self.b_matrix.T)
+        values, vectors = np.linalg.eigh(b_space)
+        # b_matrix shape is n * 3N
+        # b_space is n * n
+        basis = vectors[:, np.abs(values) > threshold]
+        # for nonlinear molecules
+        assert len(basis[0]) == self.df
+        # project out unit path vector direction
+        real_proj_mtr = np.outer(self.real_unit_path_vector, self.real_unit_path_vector)
+        # basis of space without path vector direction
+        return basis - np.dot(real_proj_mtr, basis)
 
-    @property
-    def key_ic_number(self):
-        return 1
+    # def _reduced_perturbation(self):
+    #     tsfm = np.dot(self.b_matrix, pse_inv(self.b_matrix))
+    #     result = np.dot(tsfm, self._path_vector.T)
+    #     assert len(result.shape) == 1
+    #     return result[:, None]
 
-    @property
-    def path_vector(self):
-        return self._path_vector
+    # @property
+    # def vspace(self):
+    #     """Vspace transformation matrix from internal to reduced internal
 
-    @property
-    def vspace(self):
-        """Vspace transformation matrix from internal to reduced internal
+    #     Returns
+    #     -------
+    #     vspace : np.ndarray(K, 3N - 6)
+    #     """
+    #     if self._red_space is None or self._non_red_space is None:
+    #         self._generate_reduce_space()
+    #         self._generate_nonreduce_space()
+    #         self._vspace = self._non_red_space.copy()
+    #     return self._vspace
 
-        Returns
-        -------
-        vspace : np.ndarray(K, 3N - 6)
-        """
-        if self._red_space is None or self._non_red_space is None:
-            self._generate_reduce_space()
-            self._generate_nonreduce_space()
-            self._vspace = deepcopy(self._non_red_space)
-        return self._vspace
-
+    # TO BE determined
     @classmethod
     def update_to_path_ri(cls, internal_ob, path_vector):
         assert isinstance(internal_ob, Internal)
         new_ob = deepcopy(internal_ob)
         new_ob.__class__ = cls
         new_ob._path_vector = None
+        new_ob._k_ic_n = 0
         new_ob.set_path_vector(path_vector)
         return new_ob
+
+    def set_vspace(self, new_vspace: "np.ndarray") -> None:
+        """Set vspace of system with given values
+
+        Arguments
+        ---------
+        new_vspace : np.ndarray(K, 3N - 6)
+            The new value of vspace
+        """
+        overlap = np.dot(new_vspace.T, self.path_vector)
+        if np.max(np.abs(overlap)) > 1e-6:
+            # project out the vector space
+            no_vect_path_vspace = new_vspace - np.dot(
+                np.outer(self.real_unit_path_vector, self.real_unit_path_vector),
+                new_vspace,
+            )
+            vals, vecs = np.linalg.eigh(
+                np.dot(no_vect_path_vspace, no_vect_path_vspace.T)
+            )
+            new_vspace = vecs[:, np.abs(vals) > 1e-5]
+        self._vspace = new_vspace
+        self._red_space = new_vspace[:, : self.key_ic_number]
+        self._non_red_space = new_vspace[:, self.key_ic_number :]
 
     # @property
     # def vspace(self):
--- saddle/test/test_path_ri.py
+++ saddle/test/test_path_ri.py
@@ -1,26 +1,28 @@
 import unittest
 
 import numpy as np
+from numpy.testing import assert_allclose
 from importlib_resources import path
 from saddle.ts_construct import TSConstruct
+from saddle.math_lib import diagonalize
 
 
 class TestPathRI(unittest.TestCase):
     @classmethod
     def setup_class(self):
-        with path('saddle.test.data', 'rct.xyz') as rct_path:
-            with path('saddle.test.data', 'prd.xyz') as prd_path:
+        with path("saddle.test.data", "rct.xyz") as rct_path:
+            with path("saddle.test.data", "prd.xyz") as prd_path:
                 self.ts_mol = TSConstruct.from_file(rct_path, prd_path)
-        self.ts_mol.auto_generate_ts(task='path')
+        self.ts_mol.auto_generate_ts(task="path")
         self.path_mol = self.ts_mol.ts
 
     def test_basic_property(self):
-        assert self.path_mol.key_ic_number == 1
+        assert self.path_mol.key_ic_number == 0
         try:
-            self.path_mol.set_key_ic_number(0)
+            self.path_mol.set_key_ic_number(2)
         except NotImplementedError:
             pass
-        assert self.path_mol.key_ic_number == 1
+        assert self.path_mol.key_ic_number == 0
 
         diff = self.ts_mol.prd.ic_values - self.ts_mol.rct.ic_values
         assert np.allclose(self.path_mol.path_vector, diff)
@@ -32,6 +34,39 @@ class TestPathRI(unittest.TestCase):
         assert np.linalg.norm(diff) < 1e-8
         self.path_mol._reset_v_space()
         assert self.path_mol._vspace is None
-        assert self.path_mol.key_ic_number == 1
+        assert self.path_mol.key_ic_number == 0
         diff = np.dot(self.path_mol.vspace.T, self.path_mol.path_vector)
         assert np.linalg.norm(diff) < 1e-8
+
+    def test_v_space_change(self):
+        diff = np.dot(self.path_mol.vspace.T, self.path_mol.path_vector)
+        assert np.linalg.norm(diff) < 1e-8
+        # construct vspace
+        c = np.random.rand(9, 5)
+        values, vectors = np.linalg.eigh(np.dot(c, c.T))
+        basis_vects = vectors[:, np.abs(values) > 1e-5]
+        # project out path_vector
+        no_path_space = basis_vects - np.dot(
+            np.outer(
+                self.path_mol.real_unit_path_vector, self.path_mol.real_unit_path_vector
+            ),
+            basis_vects,
+        )
+        assert (
+            np.linalg.norm(np.dot(no_path_space.T, self.path_mol.real_unit_path_vector))
+            < 1e-8
+        )
+        w, v = diagonalize(no_path_space)
+        new_v = v[:, np.abs(w) > 1e-5]
+        assert (
+            np.linalg.norm(np.dot(new_v.T, self.path_mol.real_unit_path_vector)) < 1e-8
+        )
+        # test all part are orthonormal
+        assert_allclose(np.linalg.norm(new_v, axis=0), np.ones(c.shape[1]))
+
+        # given a random vspace, generate a property one
+        self.path_mol.set_vspace(basis_vects)
+        assert_allclose(self.path_mol.vspace, new_v)
+        assert_allclose(
+            np.linalg.norm(self.path_mol.vspace, axis=0), np.ones(c.shape[1])
+        )
