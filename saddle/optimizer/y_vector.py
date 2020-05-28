27c6193acf6794460f78b352be92a30add755e5f
Derrick <yxt1991@gmail.com>
2020-05-28 15:06:23 -0400

    Update y vector template

--- saddle/optimizer/y_vector.py
+++ saddle/optimizer/y_vector.py
@@ -3,21 +3,31 @@
 
 class Point:
     """Data class to storing data for each points."""
-    def __init__(self, coords, value=None, gradient=None, hessian=None, step=None):
+
+    def __init__(
+        self, coords, value=None, gradient=None, hessian=None, step=None, y_vector=None
+    ):
         self.coords = coords
         self.value = value
         self.gradient = gradient
         self.hessian = hessian
         self.step = step
+        self.y_vector = y_vector
 
 
 class YVector:
     """Compute different y vector formula."""
 
-    _method_dict = {}
+    _method_dict = {"name": YVector._compute_y_vector_method_name}
 
-    def compute_y_vector(new_point, old_point, method=None, **kwargs):
-        return YVector._method_dict[method](new_point, old_point, **kwargs)
+    def compute_y_vector(self, points, method=None, **kwargs):
+        # points is a list of points []
+        return YVector._method_dict[method](points, old_point, **kwargs)
 
-    def _compute_y_vector_method_name(*args, **kwargs):
+    @staticmethod
+    def _compute_y_vector_method_name(points, **kwargs):
+        ...
+
+    @staticmethod
+    def _y_vector_method_name_low(args):
         ...
